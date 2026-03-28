89fa8c8187ebc6d6b35e67481d4a18cd44f10f0a
nnnnn1002 <anne1002@ewha.ac.kr>
2026-03-28 17:14:42 +0900

    첫 페이지 완성

---
+++ mini-project/src/App.jsx
@@ -0,0 +1,12 @@
+import FirstPage from './FirstPage'; // 우리가 만든 파일 가져오기
+import './App.css'; // 기본 스타일 (필요하면)
+
+function App() {
+  return (
+    <div className="App">
+      <FirstPage />
+    </div>
+  );
+}
+
+export default App;
