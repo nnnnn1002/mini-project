76133aed881205d3856db970901c6f57564795ab
nnnnn1002 <anne1002@ewha.ac.kr>
2026-03-31 12:34:38 +0900

    시작화면 완성! 다음번에는 클릭 기능 만들기!

--- mini-project/src/FirstPage.jsx
+++ mini-project/src/FirstPage.jsx
@@ -5,8 +5,8 @@ function FirstPage() {
         <div className="FirstPageContainer">
             <h1 className="Title">고향 비빔밥</h1>
             <p className="Description">고향에서 만들어 먹던 레시피를 맞혀보세요!</p>
-            <button className="Howtodo">게임 설명</button>
-            <button className="Start">게임 시작</button>
+            <button className="Howtodo-button">게임 설명</button>
+            <button className="Start-button">게임 시작</button>
         </div>
     );
 }
